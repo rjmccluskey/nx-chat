f786ad5b6d2d19aad8b047cf21c297aaa2726e61
rjmccluskey <rjmccluskey@gmail.com>
2019-05-13 19:15:05 -0700

    add basic messaging to chat socket

--- apps/server/src/app/chat/chat-socket.ts
+++ apps/server/src/app/chat/chat-socket.ts
@@ -1,8 +1,32 @@
 import * as SocketIO from 'socket.io';
 import { Server } from 'http';
+import { Message, MessageRepository } from './message-repository';
+
+export enum ChatEvent {
+  /**
+   * Emitted from client to add new message
+   */
+  addMessage = 'add message',
+  /**
+   * Emitted from server when a message has been added
+   */
+  message = 'message'
+}
 
 export function initChatSocket(server: Server) {
-  const chat = SocketIO(server).of('/chat').on('connection', socket => {
-    
+  const repository = new MessageRepository();
+  const chat = SocketIO(server).of('/chat');
+
+  chat.on('connection', socket => {
+    console.log('SocketIO: User connected to chat stream')
+
+    socket.on(ChatEvent.addMessage, async (message: Message) => {
+      console.log(`SocketIO: New message from ${message.username}`);
+      await repository.add(message);
+      chat.emit(ChatEvent.message, message);
+    });
+
+    socket.on('disconnect', () => console.log('SocketIO: User disconnected from chat stream'));
   });
+
 }
