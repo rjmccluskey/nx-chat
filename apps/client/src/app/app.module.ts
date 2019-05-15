737155c199ab3372d5eaeb9b789e28ec405b53be
rjmccluskey <rjmccluskey@gmail.com>
2019-05-14 19:24:03 -0700

    add basic routing in client

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ChatRoomComponent } from './chat-room/chat-room.component';
 import { CommonModule } from '@angular/common';
+
+const appRoutes: Routes = [
+  { path: '', component: ChatRoomComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   declarations: [AppComponent, ChatRoomComponent],
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
